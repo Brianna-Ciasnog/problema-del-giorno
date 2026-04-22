7e30635b85f6f0a207ddba7a554f985a3811205b
Brianna Ciasnog <ciasnogb1084@gmail.com>
2026-04-22 17:12:16 +0200

    github-actions-notifica

--- services/notification.js
+++ services/notification.js
@@ -1,8 +1,6 @@
-import cron from "node-cron";
 import "dotenv/config";
 import { getProblemOfTheDay, problems, startDate, today } from "../logica.js";
 /* --------- */
-cron.schedule("0 8 * * *", async () => {
   try {
     const getProblem = getProblemOfTheDay(problems, startDate, today);
     await fetch(`https://ntfy.sh/${process.env.NTFY_CHANNEL}`, {
@@ -11,5 +9,5 @@ cron.schedule("0 8 * * *", async () => {
     });
   } catch (e) {
     console.error(e);
-  }
-});
+  };
+
