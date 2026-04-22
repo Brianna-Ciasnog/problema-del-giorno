21db5e6b236c188fe252e3c68da956339c983141
Brianna Ciasnog <ciasnogb1084@gmail.com>
2026-04-22 17:29:44 +0200

    fix-notifcation-file

--- services/notification.js
+++ services/notification.js
@@ -1,15 +1,14 @@
 import "dotenv/config";
 import { getProblemOfTheDay, problems, startDate, today } from "../logica.js";
-/* --------- */
-  try {
-    const getProblem = getProblemOfTheDay(problems, startDate, today);
-    await fetch(`https://ntfy.sh/${process.env.NTFY_CHANNEL}`, {
-      method: "POST",
-      body: getProblem,
-    });
-  } catch (e) {
-    console.error(e);
-  };
 
-console.log("Problema:", getProblem);
-console.log("Canale:", process.env.NTFY_CHANNEL);
+try {
+  const getProblem = getProblemOfTheDay(problems, startDate, today);
+  console.log("Problema:", getProblem);
+  console.log("Canale:", process.env.NTFY_CHANNEL);
+  await fetch(`https://ntfy.sh/${process.env.NTFY_CHANNEL}`, {
+    method: "POST",
+    body: getProblem,
+  });
+} catch (e) {
+  console.error(e);
+}
