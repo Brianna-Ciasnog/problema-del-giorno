684145af2eec95abac037214e64b9741b10e92d7
Brianna Ciasnog <ciasnogb1084@gmail.com>
2026-04-22 17:35:06 +0200

    more-test

--- services/notification.js
+++ services/notification.js
@@ -5,15 +5,11 @@ try {
   const getProblem = getProblemOfTheDay(problems, startDate, today);
   console.log("Problema:", getProblem);
   console.log("Canale:", process.env.NTFY_CHANNEL);
-  await fetch(`https://ntfy.sh/${process.env.NTFY_CHANNEL}`, {
+  const response = await fetch(`https://ntfy.sh/${process.env.NTFY_CHANNEL}`, {
     method: "POST",
     body: getProblem,
   });
+  console.log("Status:", response.status);
 } catch (e) {
   console.error(e);
 }
-const response = await fetch(`https://ntfy.sh/${process.env.NTFY_CHANNEL}`, {
-  method: "POST",
-  body: getProblem,
-});
-console.log("Status:", response.status);
